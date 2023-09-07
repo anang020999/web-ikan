d2854eb4f0ad5b7bdee53e4f1c6bda67a2195306
anang020999 <subagiyaanang@gmail.com>
2023-09-07 18:39:11 +0700

    first commit

---
+++ Main.js
@@ -0,0 +1,57 @@
+// toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+// ketika hamburger menu diclik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+navbarNav.classList.toggle('active');
+
+
+};
+
+// Klik diluar slide mengilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e){
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+// kontak
+
+// kontan and
